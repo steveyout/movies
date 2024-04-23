d84049d870edfa2f80e3babe0d1798ca3ef7d4c2
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-04-23 13:08:29 +0300

    updated episode sources

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,17 +7,4 @@ module.exports = {
     instances:'max',
     'exec_mode':'cluster'
   }],
-
-  deploy : {
-    production : {
-      user : 'root',
-      host : '69.49.230.162',
-      ref  : 'origin/main',
-      repo : 'https://github.com/steveyout/movies.git',
-      path : '/root/movies',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
 };
