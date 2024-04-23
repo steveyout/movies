415edf7a7df1a0bee630e43a31f06c7ed12dc22a
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-04-23 13:02:04 +0300

    updated episode sources

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name:'flixhq',
     script: './node_modules/.bin/next',
-    watch: '.',
+    watch: true,
     args : "start -p 3030",
     instances:'max',
     'exec_mode':'cluster'
