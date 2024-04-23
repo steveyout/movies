2fb78b33b7262657498664b159b07532337dd17e
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-04-23 12:40:19 +0300

    updated episode sources

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   apps : [{
+    name:'flixhq',
     script: 'npm run start',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    watch: '.',
+    instances:'max',
+    exec_mode:'cluster'
   }],
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      user : 'root',
+      host : '69.49.230.162',
+      ref  : 'origin/main',
+      repo : 'https://github.com/steveyout/movies.git',
+      path : '/root/movies',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
