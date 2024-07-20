38b01fc4f6938a0252892146001fe7139fa727ea
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-07-20 13:19:39 +0300

    updated video sources

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://youplex.site',
   generateRobotsTxt: true, // (optional)
   // ...other options
-  exclude: ['/server-sitemap.xml'],
+  exclude: ['/server-sitemap.xml','middleware.js'],
   robotsTxtOptions: {
     additionalSitemaps: [
       `https://youplex.site/server-sitemap.xml`, // <==== Add here
